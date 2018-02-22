1649cd864eb7a057f704e5897caefa6fbdc6d42c
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2018-02-22 00:48:59 -0800

    Added displacement socket to material output

--- src/node_outputs.cpp
+++ src/node_outputs.cpp
@@ -8,12 +8,13 @@ CyclesShaderEditor::MaterialOutputNode::MaterialOutputNode(Point2 position)
 
 	NodeSocket* surface_input = new NodeSocket(this, SocketInOut::Input, SocketType::Closure, "Surface", "surface");
 	NodeSocket* volume_input = new NodeSocket(this, SocketInOut::Input, SocketType::Closure, "Volume", "volume");
+	NodeSocket* displacement_input = new NodeSocket(this, SocketInOut::Input, SocketType::Float, "Displacement", "displacement");
 
 	sockets.push_back(surface_input);
 	sockets.push_back(volume_input);
+	sockets.push_back(displacement_input);
 
 	type = CyclesNodeType::MaterialOutput;
-	content_width = 140.0f;
 	node_moving = false;
 
 	// This node being added should not trigger an update to undo state
