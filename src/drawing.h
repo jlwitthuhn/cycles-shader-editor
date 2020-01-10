80c0cc12c50453243c2b2f4531a16d57e54e6682
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2020-01-09 23:07:53 -0800

    Moved node drawing code to drawing.cpp

--- src/drawing.h
+++ src/drawing.h
@@ -1,7 +1,11 @@
 // Common drawing functions go here
 #pragma once
 
+#include <memory>
 #include <string>
+#include <vector>
+
+#include "util_area.h"
 
 struct NVGcontext;
 
@@ -9,12 +13,28 @@ namespace cse {
 
 	class Float2;
 	class Float3;
+	class NodeSocket;
 
 	namespace Drawing {
 
+		// UI Elements
 		void draw_button(NVGcontext* draw_context, Float2 pos, float width, float height, const std::string& label, bool enabled, bool pressed);
 		void draw_color_swatch(NVGcontext* draw_context, Float2 pos, float width, float height, Float3 color, bool selected);
 		void draw_color_pick_cursor(NVGcontext* draw_context, Float2 pos);
+
+		// Node Graph View
+		float draw_node(
+			NVGcontext* draw_context,
+			const char* title,
+			Float3 header_color,
+			float node_width,
+			Float2 world_pos,
+			bool selected,
+			const std::vector<std::shared_ptr<NodeSocket>>& socket_vec,
+			const std::shared_ptr<cse::NodeSocket>& selected_socket,
+			std::vector<HolderArea<std::weak_ptr<NodeSocket>>>& socket_targets,
+			std::vector<HolderArea<std::weak_ptr<NodeSocket>>>& label_targets
+		);
 		void draw_node_connection_curve(NVGcontext* draw_context, Float2 begin_pos, Float2 end_pos, float width);
 
 	}
--- src/node_base.cpp
+++ src/node_base.cpp
@@ -1,21 +1,17 @@
 #include "node_base.h"
 
 #include <cmath>
-#include <iomanip>
 #include <map>
-#include <sstream>
-
-#include <nanovg.h>
 
 #include "config.h"
 #include "curve.h"
+#include "drawing.h"
 #include "gui_colors.h"
 #include "gui_sizes.h"
 #include "sockets.h"
 #include "util_color_ramp.h"
 #include "util_enum.h"
 #include "util_vector.h"
-#include "wrapper_nvg_func.h"
 
 static cse::Float3 get_color_for_category(const cse::NodeCategory category)
 {
@@ -83,243 +79,20 @@ std::string cse::EditableNode::get_title() const
 
 void cse::EditableNode::draw_node(NVGcontext* const draw_context, const bool selected, const std::shared_ptr<cse::NodeSocket> selected_socket)
 {
-	float draw_pos_x = 0.0f;
-	float draw_pos_y = 0.0f;
+	const Float3 header_color = get_color_for_category(category);
 
-	content_height = sockets.size() * UI_NODE_SOCKET_ROW_HEIGHT + UI_NODE_BOTTOM_PADDING;
-
-	// Draw window
-	nvgBeginPath(draw_context);
-	nvgRoundedRect(draw_context, draw_pos_x, draw_pos_y, content_width, content_height + UI_NODE_HEADER_HEIGHT, UI_NODE_CORNER_RADIUS);
-	nvgFillColor(draw_context, nvgRGBA(180, 180, 180, 255));
-	nvgFill(draw_context);
-
-	// Draw header
-	{
-		const float rect_pos_y = draw_pos_y + UI_NODE_HEADER_HEIGHT - UI_NODE_CORNER_RADIUS;
-		nvgBeginPath(draw_context);
-		nvgRoundedRect(draw_context, draw_pos_x, draw_pos_y, content_width, UI_NODE_HEADER_HEIGHT, UI_NODE_CORNER_RADIUS);
-		nvgRect(draw_context, draw_pos_x, rect_pos_y, content_width, UI_NODE_CORNER_RADIUS);
-		nvgFillColor(draw_context, get_color_for_category(category));
-		nvgFill(draw_context);
-	}
-
-	// Draw border
-	nvgBeginPath(draw_context);
-	nvgRoundedRect(draw_context, draw_pos_x, draw_pos_y, content_width, content_height + UI_NODE_HEADER_HEIGHT, UI_NODE_CORNER_RADIUS);
-	if (selected) {
-		nvgStrokeColor(draw_context, nvgRGBA(255, 255, 255, 225));
-	}
-	else {
-		nvgStrokeColor(draw_context, nvgRGBA(0, 0, 0, 225));
-	}
-	nvgStrokeWidth(draw_context, 1.5f);
-	nvgStroke(draw_context);
-
-	// Title
-	nvgFontSize(draw_context, UI_FONT_SIZE_NORMAL);
-	nvgFontFace(draw_context, "sans");
-	nvgTextAlign(draw_context, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-	nvgFontBlur(draw_context, 0.0f);
-	nvgFillColor(draw_context, nvgRGBA(0, 0, 0, 255));
-	nvgText(draw_context, draw_pos_x + content_width / 2, draw_pos_y + UI_NODE_HEADER_HEIGHT / 2, title.c_str(), nullptr);
-
-	float next_draw_y = draw_pos_y + UI_NODE_HEADER_HEIGHT + 2.0f;
-	// Sockets
-	label_targets.clear();
-	socket_targets.clear();
-	for (const auto this_socket: sockets) {
-		// Generate the text that will be used on this socket's label
-		std::string label_text;
-		std::string text_before_crossout; // For measuring text size later
-		if (this_socket->value.use_count() > 0) {
-			text_before_crossout = this_socket->display_name + ":";
-			if (this_socket->socket_type == SocketType::FLOAT) {
-				const std::shared_ptr<FloatSocketValue> float_val = std::dynamic_pointer_cast<FloatSocketValue>(this_socket->value);
-				if (float_val) {
-					std::stringstream label_string_stream;
-					label_string_stream << this_socket->display_name << ": " << std::fixed << std::setprecision(3) << float_val->get_value();
-					label_text = label_string_stream.str();
-				}
-			}
-			else if (this_socket->socket_type == SocketType::VECTOR) {
-				if (this_socket->selectable == true) {
-					label_text = this_socket->display_name + ": [Vector]";
-				}
-				else {
-					label_text = this_socket->display_name;
-				}
-			}
-			else if (this_socket->socket_type == SocketType::COLOR) {
-				std::stringstream label_string_stream;
-				label_string_stream << this_socket->display_name << ": ";
-				label_text = label_string_stream.str();
-			}
-			else if (this_socket->socket_type == SocketType::STRING_ENUM) {
-				label_text = this_socket->display_name + ": [Enum]";
-			}
-			else if (this_socket->socket_type == SocketType::INT) {
-				const std::shared_ptr<IntSocketValue> int_val = std::dynamic_pointer_cast<IntSocketValue>(this_socket->value);
-				if (int_val) {
-					std::stringstream label_string_stream;
-					label_string_stream << this_socket->display_name << ": " << std::fixed << std::setprecision(3) << int_val->get_value();
-					label_text = label_string_stream.str();
-				}
-			}
-
-			else if (this_socket->socket_type == SocketType::BOOLEAN) {
-				const std::shared_ptr<BoolSocketValue> bool_val = std::dynamic_pointer_cast<BoolSocketValue>(this_socket->value);
-				if (bool_val) {
-					if (bool_val->value) {
-						label_text = this_socket->display_name + ": True";
-					}
-					else {
-						label_text = this_socket->display_name + ": False";
-					}
-				}
-			}
-			else if (this_socket->socket_type == SocketType::CURVE) {
-				label_text = this_socket->display_name + ": [Curve]";
-			}
-			else {
-				label_text = this_socket->display_name;
-			}
-		}
-		else {
-			label_text = this_socket->display_name;
-		}
-
-		// Draw highlight if this node is selected
-		if (this_socket == selected_socket) {
-			nvgBeginPath(draw_context);
-			nvgRoundedRect(draw_context, 4.0f, next_draw_y, content_width - 8.0f, UI_NODE_SOCKET_ROW_HEIGHT, 0.0f);
-			nvgFillColor(draw_context, nvgRGBA(210, 210, 210, 255));
-			nvgFill(draw_context);
-		}
-
-		// Draw label
-		nvgFontSize(draw_context, UI_FONT_SIZE_NORMAL);
-		nvgTextAlign(draw_context, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-		nvgFontBlur(draw_context, 0.0f);
-		nvgFillColor(draw_context, nvgRGBA(0, 0, 0, 255));
-		nvgFontFace(draw_context, "sans");
-
-		if (this_socket->value.use_count() > 0 && this_socket->socket_type == SocketType::COLOR) {
-			const float SWATCH_HEIGHT = 14.0f;
-			const float SWATCH_WIDTH = 24.0f;
-			const float SWATCH_CORNER_RADIUS = 8.0f;
-
-			float bounds[4];
-			nvgTextBounds(draw_context, 0.0f, 0.0f, label_text.c_str(), nullptr, bounds);
-			const float label_width = bounds[2] - bounds[0];
-
-			const float text_pos_x = draw_pos_x + content_width / 2 - SWATCH_WIDTH / 2;
-			const float text_pos_y = next_draw_y + UI_NODE_SOCKET_ROW_HEIGHT / 2;
-			nvgText(draw_context, text_pos_x, text_pos_y, label_text.c_str(), nullptr);
-
-			const Float3 swatch_color = dynamic_cast<ColorSocketValue*>(this_socket->value.get())->get_value();
-
-			const float swatch_pos_x = text_pos_x + label_width / 2 + 1.0f;
-			const float swatch_pos_y = next_draw_y + (UI_NODE_SOCKET_ROW_HEIGHT - SWATCH_HEIGHT) / 2;
-
-			nvgBeginPath(draw_context);
-			nvgRoundedRect(draw_context, swatch_pos_x, swatch_pos_y, SWATCH_WIDTH, SWATCH_HEIGHT, SWATCH_CORNER_RADIUS);
-			nvgFillColor(draw_context, swatch_color);
-			nvgFill(draw_context);
-			nvgStrokeWidth(draw_context, 1.0f);
-			nvgStrokeColor(draw_context, nvgRGBA(0, 0, 0, 255));
-			nvgStroke(draw_context);
-
-			if (this_socket->input_connected_this_frame && this_socket->value.use_count() > 0) {
-				const float x1 = swatch_pos_x - 3.0f;
-				const float x2 = swatch_pos_x  + SWATCH_WIDTH + 3.0f;
-				const float y = swatch_pos_y + SWATCH_HEIGHT / 2.0f;
-				nvgBeginPath(draw_context);
-				nvgMoveTo(draw_context, x1, y);
-				nvgLineTo(draw_context, x2, y);
-				nvgStrokeWidth(draw_context, 1.2f);
-				nvgStroke(draw_context);
-				this_socket->input_connected_this_frame = false;
-			}
-		}
-		else {
-			const float text_pos_x = draw_pos_x + content_width / 2;
-			const float text_pos_y = next_draw_y + UI_NODE_SOCKET_ROW_HEIGHT / 2;
-			nvgText(draw_context, text_pos_x, text_pos_y, label_text.c_str(), nullptr);
-			if (this_socket->input_connected_this_frame && this_socket->value.use_count() > 0) {
-				// Output is [xmin, ymin, xmax, ymax]
-				float full_size[4];
-				float short_size[4];
-				nvgTextBounds(draw_context, text_pos_x, text_pos_y, label_text.c_str(), nullptr, full_size);
-				nvgTextBounds(draw_context, text_pos_x, text_pos_y, text_before_crossout.c_str(), nullptr, short_size);
-				const float short_width = short_size[2] - short_size[0];
-				const float x1 = full_size[0] + short_width + 1.0f;
-				const float x2 = full_size[2] + 2.0f;
-				nvgBeginPath(draw_context);
-				nvgMoveTo(draw_context, x1, text_pos_y);
-				nvgLineTo(draw_context, x2, text_pos_y);
-				nvgStrokeWidth(draw_context, 1.2f);
-				nvgStroke(draw_context);
-				this_socket->input_connected_this_frame = false;
-			}
-		}
-
-		if (this_socket->selectable) {
-			// Add label click target
-			const cse::Float2 click_target_begin(0, next_draw_y - draw_pos_y);
-			const cse::Float2 click_target_end(content_width, next_draw_y - draw_pos_y + UI_NODE_SOCKET_ROW_HEIGHT);
-			const HolderArea<std::weak_ptr<NodeSocket>> label_target(click_target_begin, click_target_end, this_socket);
-			label_targets.push_back(label_target);
-		}
-
-		if (this_socket->draw_socket) {
-			cse::Float2 socket_position;
-			if (this_socket->io_type == SocketIOType::INPUT) {
-				socket_position = cse::Float2(draw_pos_x, next_draw_y + UI_NODE_SOCKET_ROW_HEIGHT / 2);
-			}
-			else {
-				socket_position = cse::Float2(draw_pos_x + content_width, next_draw_y + UI_NODE_SOCKET_ROW_HEIGHT / 2);
-			}
-			nvgBeginPath(draw_context);
-			nvgCircle(draw_context, socket_position.x, socket_position.y, UI_NODE_SOCKET_RADIUS);
-
-			this_socket->world_draw_position = world_pos + socket_position;
-
-			// Add click target for this socket
-			cse::Float2 local_socket_position(socket_position.x - draw_pos_x, socket_position.y - draw_pos_y);
-			cse::Float2 click_target_begin(local_socket_position.x - 7.0f, local_socket_position.y - 7.0f);
-			cse::Float2 click_target_end(local_socket_position.x + 7.0f, local_socket_position.y + 7.0f);
-			const HolderArea<std::weak_ptr<NodeSocket>> socket_target(click_target_begin, click_target_end, this_socket);
-			socket_targets.push_back(socket_target);
-
-			if (this_socket->socket_type == SocketType::CLOSURE) {
-				nvgFillColor(draw_context, nvgRGBA(100, 200, 100, 255));
-			}
-			else if (this_socket->socket_type == SocketType::COLOR) {
-				nvgFillColor(draw_context, nvgRGBA(200, 200, 42, 255));
-			}
-			else if (this_socket->socket_type == SocketType::FLOAT) {
-				nvgFillColor(draw_context, nvgRGBA(240, 240, 240, 255));
-			}
-			else if (this_socket->socket_type == SocketType::NORMAL) {
-				nvgFillColor(draw_context, nvgRGBA(100, 100, 200, 255));
-			}
-			else if (this_socket->socket_type == SocketType::VECTOR) {
-				nvgFillColor(draw_context, nvgRGBA(100, 100, 200, 255));
-			}
-			else {
-				nvgFillColor(draw_context, nvgRGBA(255, 0, 0, 255));
-			}
-
-			nvgFill(draw_context);
-
-			nvgStrokeColor(draw_context, nvgRGBf(0.0f, 0.0f, 0.0f));
-			nvgStrokeWidth(draw_context, 1.0f);
-			nvgStroke(draw_context);
-		}
-
-		next_draw_y += UI_NODE_SOCKET_ROW_HEIGHT;
-	}
+	content_height = Drawing::draw_node(
+		draw_context,
+		title.c_str(),
+		header_color,
+		content_width,
+		world_pos,
+		selected,
+		sockets,
+		selected_socket,
+		socket_targets,
+		label_targets
+	);
 }
 
 bool cse::EditableNode::contains_point(const Float2 world_pos_in) const
